3caadcd4910e603406e83b0cc6212336cc03b874
haonx <qtvhao@gmail.com>
2017-03-09 00:28:38 +0700

    add example for set default `relations.user_groups.0.privileges`

---
+++ User.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: qtvha
+ * Date: 3/9/2017
+ * Time: 12:09 AM
+ */
+class User
+{
+    public $relations;
+
+    public function __construct()
+    {
+        data_set($this, 'relations.user_groups.0.privileges', [
+            'create',
+            'read',
+            'update',
+            'delete'
+        ]);
+    }
+}
