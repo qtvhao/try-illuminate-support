85e601e1f50a3fbaf6a96cb197d27ed6f3dd3879
haonx <qtvhao@gmail.com>
2017-03-18 15:19:32 +0700

    use display_name

--- User.php
+++ User.php
@@ -15,7 +15,7 @@ class User implements \Illuminate\Contracts\Support\Arrayable
     {
         $default_privileges = ['create', 'read', 'update', 'delete'];
         data($this)->set('relations.user_groups.0.privileges', $default_privileges);
-        data($this)->set('display_name', str_random(5));
+        data($this)->set('display_name', 'haonx '.str_random(5));
     }
 
     public function hasPrivilege($privilege)
