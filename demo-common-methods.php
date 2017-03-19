07d0c525fd4e063b882cd49ce4aab9ce0c8e3d90
haonx <qtvhao@gmail.com>
2017-03-19 21:19:28 +0700

    only use needed methods

--- demo-common-methods.php
+++ demo-common-methods.php
@@ -20,22 +20,16 @@ for ($i = 0; $i < 10; $i++) {
 $items = $all_items
     ->each(function ($item) {/*echo $item;*/
     })
-    // map & filter is equals
+    // Cú pháp cho các hàm map & filter & reject cũng như vậy
     ->push(new User())
-    ->reject(function () {
-        return false;
-    })
 ;
 
 #GET ITEM
 print_r($all_items[2]);
-print_r($all_items->search('value'));
-print_r($all_items->search(function () {
-    return true;
-}));
 print_r($all_items->all());
 print_r($items->implode(''));
+
 #CHECKER
 print_r($all_items->isEmpty());
 print_r($all_items->every(function () {return true;}));
-print_r($all_items->contains('value'));
+print_r($all_items->pluck('display_name')->contains('value'));
