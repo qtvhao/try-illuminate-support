1119e0aae7d377febad23a0288a63ec43ada0639
haonx <qtvhao@gmail.com>
2017-03-19 21:01:08 +0700

    only use needed methods

--- demo-common-methods.php
+++ demo-common-methods.php
@@ -18,34 +18,24 @@ for ($i = 0; $i < 10; $i++) {
 
 #return self
 $items = $all_items
-    ->filter(function () {
-        return true;
-    })
-    ->map(function ($item) {
-        return $item;
-    })
     ->each(function ($item) {/*echo $item;*/
     })
+    // map & filter is equals
     ->push(new User())
-    ->put('custom_key', new User())
     ->reject(function () {
         return false;
     })
 ;
 
 #GET ITEM
-print_r($all_items->get(2));
 print_r($all_items[2]);
 print_r($all_items->search('value'));
 print_r($all_items->search(function () {
     return true;
 }));
 print_r($all_items->all());
-print_r($all_items->toArray());
 print_r($items->implode(''));
-print_r($all_items->toJson());
-print_r((string)$items);
 #CHECKER
 print_r($all_items->isEmpty());
 print_r($all_items->every(function () {return true;}));
-print_r($all_items->has(3));
+print_r($all_items->contains('value'));
