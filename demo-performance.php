e11c3a969695f8b0241ae4f46558ed9c41d74acf
haonx <qtvhao@gmail.com>
2017-03-20 07:09:59 +0700

    add performance demo

---
+++ demo-performance.php
@@ -0,0 +1,14 @@
+<?php
+require_once 'vendor/autoload.php';
+new \Illuminate\Support\Collection();
+//we calculating how many an instance of Collection exceeded in-memory
+$start = memory_get_usage();
+$users = collect();
+$end = memory_get_usage();
+var_export($end-$start); #the memory amount in bytes = 312 bytes
+
+//we calculating how many an instance of Collection time to new
+$start = microtime(1);
+$users = collect()->pluck('a')->chunk(3);
+$end = microtime(1);
+var_export(($end-$start));#time to new Collection
