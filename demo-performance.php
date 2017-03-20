819a006b6af3b33711eeb9bb4f58dc3a6b2d6d07
haonx <qtvhao@gmail.com>
2017-03-20 07:34:05 +0700

    add performance demo

--- demo-performance.php
+++ demo-performance.php
@@ -1,6 +1,11 @@
 <?php
 require_once 'vendor/autoload.php';
+$start = microtime(1);
 new \Illuminate\Support\Collection();
+$end = microtime(1);
+var_export(($end - $start)*1e3);
+#time to load library in ms 1.8 - 2.5ms
+
 //we calculating how many an instance of Collection exceeded in-memory
 $start = memory_get_usage();
 $users = collect();
