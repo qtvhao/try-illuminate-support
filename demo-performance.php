093d1afea4da3f1ffc34af53f2b103278cf4e5ed
haonx <qtvhao@gmail.com>
2017-03-20 07:17:56 +0700

    add performance demo

--- demo-performance.php
+++ demo-performance.php
@@ -5,10 +5,12 @@ new \Illuminate\Support\Collection();
 $start = memory_get_usage();
 $users = collect();
 $end = memory_get_usage();
-var_export($end-$start); #the memory amount in bytes = 312 bytes
+var_export($end - $start); #the memory amount in bytes = 312 bytes
 
 //we calculating how many an instance of Collection time to new
 $start = microtime(1);
-$users = collect()->pluck('a')->chunk(3);
+for ($i=0;$i<50;$i++){
+    collect();
+}
 $end = microtime(1);
-var_export(($end-$start));#time to new Collection
+var_export(($end - $start));#time to new Collection 3ms
