302c3702c7c62eeb03a7e1e129e170a1402b2cd5
haonx <haonx@egdgroup.com>
2017-04-01 09:00:28 +0700

    add demo-hom

--- demo-hom.php
+++ demo-hom.php
@@ -5,9 +5,15 @@
  * Date: 3/31/2017
  * Time: 7:16 PM
  */
+use Illuminate\Support\Collection;
+
 require_once 'vendor/autoload.php';
 #Higher Order Messaging
 seed_users()->each->addToUserGroup();
 seed_users()->every->sendEmail('title', 'content');
 seed_users()->filter->isVip()->each->sendEmailInvite();
 seed_users()->filter->isPaidUser()->each->sendThankYou();
+/**
+ * @var Collection $posts
+ */
+$titles = $posts->map->title;
